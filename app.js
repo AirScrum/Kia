d6f8d6503e6635b20b6091afba9ea1aa1df51654
Shehab Adel <shehabeldinadel@gmail.com>
2022-10-30 01:22:50 +0200

    Trying RabbitMQ with Node.js

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
 const express = require('express')
 const httpProxy = require('express-http-proxy')
+var amqp = require('amqplib/callback_api');
+
 const app = express()
+app.use(express.json());
 
 const port = 4000
 
-const userServiceProxy = httpProxy('http://localhost:3000/')
-const userServiceProxy2 = httpProxy('http://localhost:8001/')
-
-// Authentication
-app.use((req, res, next) => {
-  // TODO: my authentication logic
-  next()
-})
-
 // Proxy request
 app.get('/', (req, res, next) => {
-  userServiceProxy(req, res, next)
+  //Extract the message
+  const { message } = req.body
+  //Connect to RabbitMQ
+  amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = 'hello';
+      //Send to queue
+      channel.sendToQueue(queue, Buffer.from(message));
+      console.log(" [x] Sent %s", message);
+    });
+  });
+
 })
 
 app.get('/u', (req, res, next) => {
-  userServiceProxy(req, res, next)
-})
+  amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = 'hello';
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+      //Read from queue
+      channel.consume(queue, function (msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+      }, {
+        noAck: true
+      });
+    });
+  });
 
-app.get('/main', (req, res, next) => {
-  userServiceProxy2(req, res, next)
 })
 
 app.listen(port, () => {
