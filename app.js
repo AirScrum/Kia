fefeb1f3657e636e345f21e3262062dd4a39075c
Shehab Adel <shehabeldinadel@gmail.com>
2022-12-05 12:00:02 +0200

    Modified app.js routing, and added several comments. Installed babel

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 4000
 
 const userServiceProxy = httpProxy('http://localhost:3000/')
-const userServiceProxy2 = httpProxy('http://localhost:8001/')
+const userServiceProxy2 = httpProxy('http://localhost:8002/')
 
 // Authentication
 app.use((req, res, next) => {
@@ -18,11 +18,13 @@ app.get('/', (req, res, next) => {
   userServiceProxy(req, res, next)
 })
 
-app.get('/u', (req, res, next) => {
+//Route the request to the Speech2Text service
+app.get('/request/speech2text', (req, res, next) => {
   userServiceProxy(req, res, next)
 })
 
-app.get('/main', (req, res, next) => {
+//Route request to the Processing service
+app.post('/request/process', (req, res, next) => {
   userServiceProxy2(req, res, next)
 })
 
