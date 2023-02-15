ca41f9f6df7c8b80473585d064dc7bf1b02d49ab
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2023-02-15 17:57:12 +0200

    Add database schema and mongodb Add .env Add dependencies Use passportJs to implement create and login routes

---
+++ app2.js
@@ -0,0 +1,161 @@
+//Requires
+const express = require('express')
+const httpProxy = require('express-http-proxy')
+var cors = require('cors')
+const mongoose = require('mongoose');
+var axios = require("axios").default;
+const dotenv=require('dotenv').config();
+const userStoriesData = require('./utils/constants').userStories;
+const { hashSync, compareSync } = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+var myPassportService = require('./config/passport');
+
+//Schema requires
+const UserModel = require('./models/user');
+
+// Instances
+const app = express()
+const userServiceProxy = httpProxy('http://localhost:3000/')
+const userServiceProxy2 = httpProxy('http://localhost:8002/')
+
+// Middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+//To connect to database
+const dbURI=process.env.MONGO_DB_URI;
+
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        app.listen(process.env.PORT || 4000)
+        console.log(`Example app listening on port ${process.env.PORT}`)
+    }
+    )
+    .catch((err) => console.log(err));
+
+
+
+/**
+ * 
+ * Account Management Routes
+ * 
+ */
+
+// To register
+app.post('/register', (req, res) => {
+    const user = new UserModel({
+        email: req.body.email,
+        password: hashSync(req.body.password, 10)
+    })
+
+    user.save().then(user => {
+        res.send({
+            success: true,
+            message: "User created successfully.",
+            user: {
+                id: user._id,
+                email: user.email
+            }
+        })
+    }).catch(err => {
+        res.send({
+            success: false,
+            message: "Something went wrong",
+            error: err
+        })
+    })
+})
+
+// To login
+app.post('/login', (req, res) => {
+    UserModel.findOne({ email: req.body.email }).then(user => {
+        //No user found
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "Could not find the user."
+            })
+        }
+
+        //Incorrect password
+        if (!compareSync(req.body.password, user.password)) {
+            return res.status(401).send({
+                success: false,
+                message: "Incorrect password"
+            })
+        }
+
+        const payload = {
+            username: user.email,
+            id: user._id
+        }
+
+        const token = jwt.sign(payload, "Airscrum Secret String", { expiresIn: "10h" })
+
+        return res.status(200).send({
+            success: true,
+            message: "Logged in successfully!",
+            token: "Bearer " + token
+        })
+    })
+})
+
+// Example of protected routes
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.status(200).send({
+        success: true,
+        user: {
+            id: req.user._id,
+            username: req.user.email,
+        }
+    })
+})
+
+
+
+/**
+ * 
+ * Other Routes
+ * 
+ */
+
+// Proxy request
+app.get('/', (req, res, next) => {
+  userServiceProxy(req, res, next)
+})
+
+//Route the request to the Speech2Text service
+app.get('/request/speech2text', (req, res, next) => {
+  userServiceProxy(req, res, next)
+})
+
+/**
+ * @author Shehab Adel
+ * @summary A prototype middleware to return static user stories
+ */
+app.post('/userstories',async (req,res,next)=>{
+  try {
+    //TODO Delete this after prototype presentation lol
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    res.json({data:userStoriesData}).status(200).send();
+  } catch (error) {
+    console.error(error)
+    res.status(500).end();
+  }
+})
+
+//Route request to the Processing service
+app.post('/request/process', (req, res, next) => {
+  userServiceProxy2(req, res, next)
+})
---
+++ config/passport.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const opts = {}
+const UserModel = require('../models/user');
+const passport = require('passport')
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'Airscrum Secret String';
+
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+    UserModel.findOne({ id: jwt_payload.id }, function (err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
