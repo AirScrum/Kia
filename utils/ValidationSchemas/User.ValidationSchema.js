c2a7a6fbb8d4cdbfd10fccf66e2d7f8177a2c56f
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-03-12 16:37:59 +0200

    Removed the minDomainSegement attribute from the email

--- utils/ValidationSchemas/User.ValidationSchema.js
+++ utils/ValidationSchemas/User.ValidationSchema.js
@@ -3,11 +3,7 @@ const Joi = require("joi");
 const userValidSchemaRegister = Joi.object({
   fullName: Joi.string().trim().required(),
   password: Joi.string().min(12).max(15).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2 })
-    .trim()
-    .lowercase()
-    .required(),
+  email: Joi.string().email().trim().lowercase().required(),
   birthDate: Joi.string().isoDate().trim(),
 });
 module.exports = { userValidSchemaRegister };
