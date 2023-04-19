55c1bd28a56ce513126bd1c0c3ad9efbef780609
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-04-20 00:10:29 +0200

    Implemented deleting a UserStory

--- resources/UserStory/UserStory.controller.js
+++ resources/UserStory/UserStory.controller.js
@@ -26,6 +26,32 @@ const updateUserStory = async (req, res, next) => {
     }
   }
 };
+const deleteUserStory = async (req, res, next) => {
+  try {
+    const userStoryID = req.params.id;
+    const response = await axios.delete(
+      `${process.env.USER_MANAGEMENT_URL}api/userstories/${userStoryID}/`
+    );
+    if (response.status === 200) {
+      return res.status(200).send(response.data);
+    } else {
+      return res.status(response.status).send(response.data);
+    }
+  } catch (error) {
+    console.error(error.message);
+    if (error?.response) {
+      console.log(error.response?.status);
+      return res
+        .status(error.response?.status)
+        .send(error.response?.data.error);
+    } else if (error?.request) {
+      return res.status(500).send(error.request);
+    } else {
+      res.status(500).send(error.message);
+    }
+  }
+};
 module.exports = {
   updateUserStory,
+  deleteUserStory,
 };
