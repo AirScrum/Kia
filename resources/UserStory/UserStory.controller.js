b1f672dd1546950dd70a644169d8a5d8ffb76883
Shehab El Din Adel <53188087+shehabadel@users.noreply.github.com>
2023-04-20 03:16:31 +0200

    Implemented creating UserStory logic

--- resources/UserStory/UserStory.controller.js
+++ resources/UserStory/UserStory.controller.js
@@ -51,7 +51,34 @@ const deleteUserStory = async (req, res, next) => {
     }
   }
 };
+const createUserStory = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const response = await axios.post(
+      `${process.env.USER_MANAGEMENT_URL}api/userstories/`,
+      data
+    );
+    if (response.status === 201) {
+      return res.status(201).send(response.data);
+    } else {
+      return res.status(response.status).send(response.data);
+    }
+  } catch (error) {
+    console.error(error.message);
+    if (error?.response) {
+      console.log(error.response?.status);
+      return res
+        .status(error.response?.status)
+        .send(error.response?.data.error);
+    } else if (error?.request) {
+      return res.status(500).send(error.request);
+    } else {
+      res.status(500).send(error.message);
+    }
+  }
+};
 module.exports = {
   updateUserStory,
   deleteUserStory,
+  createUserStory,
 };
