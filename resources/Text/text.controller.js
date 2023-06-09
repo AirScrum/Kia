8609be9b96eedbb64da087b03d2ecb60a19dfefd
Shehab Adel <shehabeldinadel@gmail.com>
2023-06-09 19:30:08 +0300

    Added a route for regex searching

--- resources/Text/text.controller.js
+++ resources/Text/text.controller.js
@@ -31,7 +31,7 @@ const getMeetingMetaData = async (req, res, next) => {
     console.log(req.params.meetingID);
     const meetingID = req.params.meetingID;
     var response = await axios.post(
-      `${process.env.USER_MANAGEMENT_URL}api/texts/${meetingID}/`,
+      `${process.env.USER_MANAGEMENT_URL}api/texts/id/${meetingID}/`,
       {
         userID: req.user.id,
         meetingID: meetingID,
@@ -57,7 +57,39 @@ const getMeetingMetaData = async (req, res, next) => {
     }
   }
 };
+const getMeetingByIDRegex = async (req, res, next) => {
+  try {
+    console.log(req.params.idRegex);
+    const idRegex = req.params.idRegex;
+    var response = await axios.post(
+      `${process.env.USER_MANAGEMENT_URL}api/texts/search/`,
+      {
+        userID: req.user.id,
+        regex: idRegex,
+      }
+    );
+    console.log(response.data);
+    if (response.status === 200) {
+      return res.status(200).send(response.data);
+    } else {
+      return res.status(response.status).send(response.data);
+    }
+  } catch (error) {
+    console.error(error.message);
+    if (error?.response) {
+      console.log(error.response?.status);
+      return res
+        .status(error.response?.status)
+        .send(error.response?.data.error);
+    } else if (error?.request) {
+      return res.status(500).send(error.request);
+    } else {
+      res.status(500).send(error.message);
+    }
+  }
+};
 module.exports = {
   deleteMeeting,
   getMeetingMetaData,
+  getMeetingByIDRegex
 };
